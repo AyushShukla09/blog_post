8e4ee5ef31c62d853746d5e0e4b45aca0dc1b15c
AyushShukla09 <hbkayushshukla09@gmail.com>
2025-03-17 00:25:28 +0530

    Updated configuration to host on render

--- docs/docs.go
+++ docs/docs.go
@@ -309,9 +309,9 @@ const docTemplate = `{
 // SwaggerInfo holds exported Swagger Info so clients can modify it
 var SwaggerInfo = &swag.Spec{
 	Version:          "1.0",
-	Host:             "localhost:8080",
+	Host:             "quartiz-blog-post.onrender.com",
 	BasePath:         "/api/v1",
-	Schemes:          []string{},
+	Schemes:          []string{"https"},
 	Title:            "Blog API",
 	Description:      "",
 	InfoInstanceName: "swagger",
--- main.go
+++ main.go
@@ -10,6 +10,7 @@ import (
 	"github.com/gofiber/fiber/v2/log"
 	"github.com/gofiber/fiber/v2/middleware/cors"
 	"github.com/gofiber/swagger" // swagger handler
+	"github.com/spf13/viper"
 )
 
 //	@title	Blog API
@@ -17,18 +18,38 @@ import (
 // @version		1.0
 // @contact.name	Ayush Shukla
 // @contact.email	ayush.shukla8797@gmail.com
-// @host			localhost:8080
+// @host			quartiz-blog-post.onrender.com
 // @BasePath		/api/v1
+// @Schemes https
 func main() {
+	initConfig()
+	siteURL := viper.GetString("SITE_URL")
+	if siteURL == "" {
+		log.Error("SITE_URL is not set. Please configure it in .env file or environment variables.")
+	}
+	port := viper.GetString("PORT")
+	if port == "" {
+		port = "8080"
+	}
+
 	app := setup()
-	log.Info("Listening at port :8080")
-	log.Fatal(app.Listen(":8080"))
+	log.Info("Listening at port: " + siteURL + port)
+	log.Fatal(app.Listen(":" + port))
+}
+
+func initConfig() {
+	viper.SetConfigFile(".env")
+	viper.AutomaticEnv()
+
+	if err := viper.ReadInConfig(); err != nil {
+		log.Error("Error reading config file: %v", err)
+	}
 }
 
 func setup() *fiber.App {
 	app := fiber.New()
 	app.Use(cors.New(cors.Config{
-		AllowOrigins: "*", 
+		AllowOrigins: "*",
 		AllowMethods: "GET,POST,PUT,DELETE,PATCH",
 		AllowHeaders: "*",
 	}))
